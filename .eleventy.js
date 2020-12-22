e43e66aa0b5322792bfd2ae9e7941d6c676d1607
Pascal Mueller <paesku@gmail.com>
2020-12-22 14:14:29 +0100

    setup img copy and scss

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,10 @@
-module.exports = function () {
-  return {};
+const pluginSass = require("eleventy-plugin-sass");
+const sassPluginOptions = {
+  watch: ['_sass/**/*.{scss,sass}'],
 };
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPlugin(pluginSass, sassPluginOptions);
+  eleventyConfig.addPassthroughCopy({ 'assets/*.jpg': 'img' });
+  eleventyConfig.addPassthroughCopy({ 'assets/portfolio/img/*.jpg': 'img/portfolio' });
+};
+
