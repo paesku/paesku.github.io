caae93b22da4f34f4d72d647db454477c24263cb
Pascal Mueller <paesku@gmail.com>
2020-12-22 21:58:41 +0100

    clean folder structure

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,16 @@
 const pluginSass = require("eleventy-plugin-sass");
 const sassPluginOptions = {
-  watch: ['_sass/**/*.{scss,sass}'],
+  watch: ['src/styles/**/*.{scss,sass}'],
 };
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(pluginSass, sassPluginOptions);
-  eleventyConfig.addPassthroughCopy({ 'assets/*.(jpg|png)': 'img' });
-  eleventyConfig.addPassthroughCopy({ 'assets/portfolio/img/*.(jpg|png)': 'img/portfolio' });
+  eleventyConfig.addPassthroughCopy({ 'src/assets/*.(jpg|png)': 'img' });
+  eleventyConfig.addPassthroughCopy({ 'src/assets/portfolio/img/*.(jpg|png)': 'img/portfolio' });
+
+  return {
+    dir: {
+      input: 'src',
+    }
+  };
 };
 
