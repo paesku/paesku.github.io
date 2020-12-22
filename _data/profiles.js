162a79f8b2bbd2e7356e194d4df0a8e6fc144a24
Pascal Mueller <paesku@gmail.com>
2020-12-22 17:08:13 +0100

    manage footer profiles

---
+++ _data/profiles.js
@@ -0,0 +1,10 @@
+module.exports = function () {
+  return [
+    {url: 'https://xing.com/profile/Pascal_Mueller73', icon: 'xing'},
+    {url: 'https://linkedin.com/in/pascal--mueller', icon: 'linkedin'},
+    {url: 'https://stackshare.io/paesku', icon: 'stackshare'},
+    {url: 'https://github.com/paesku', icon: 'github'},
+    {url: 'https://twitter.com/paesku', icon: 'twitter'},
+    {url: 'https://keybase.io/paesku', icon: 'keybase'},
+  ];
+}
